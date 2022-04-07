0e61f880a265903333a7e018fef570dea8cb56e3
Uziel Moreira dos Santos <103126069+UzielJunior@users.noreply.github.com>
2022-04-07 15:06:37 -0300

    add pastas
    adicionando as pastas do mundo 1 e 2

---
+++ Mundo_1/#002_Respondendo ao Usuário.py
@@ -0,0 +1,3 @@
+
+nome = str(input('Digite seu nome:'))
+print('É um prazer {}'.format(nome))
---
+++ Mundo_1/#004.py
@@ -0,0 +1,9 @@
+
+a = input('digite algo:')
+
+print('o tipo é:',type(a))
+print('o tipo éminusculo:',a.islower())
+print('o tipo é maiusculo:',a.isupper())
+print('o tipo é numero:',a.isnumeric())
+print('o tipo é alfabetico :',a.isalpha())
+print('o tipo é alfanumerico :',a.isalnum())
